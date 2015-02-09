78d9660723daa1c87bd6f51c184ade679a2cecd4
Jacob Soo <jacobsoo@users.noreply.github.com>
2015-02-09 22:37:28 +0800

    Updates

---
+++ Bushido/common/curl.py
@@ -0,0 +1,96 @@
+import urllib, urllib2, time, os, socket
+import xml.dom.minidom
+
+from Bushido.common.out import _log
+
+class curl:
+    def __init__(self):
+        self.name = None
+        self.path = None
+        self.cookie = None
+
+    #---------------------------------------------------
+    # _curl : Grab the file based on url
+    #---------------------------------------------------
+    def _curl(self, url, **httpheaders):
+        retry = 3
+        while retry > 0:
+            try:
+                return self._do_curl(url, **httpheaders)
+            except StandardError:
+                _log('[-] Retry...')
+            retry = retry - 1
+        _log('[-] Failed after retry %d times.' % retry)
+        return ("","")
+        #raise StandardError('[-] Failed after retry %d times.' % retry)
+
+    #---------------------------------------------------
+    # _do_curl : Grab the file contents based on url
+    #            Function will return filename and file contents
+    #---------------------------------------------------
+    def _do_curl(self, url, **httpheaders):
+        #' get html text from url. '
+        headers = {
+                #'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0',
+                'Referer': url
+        }
+        headers.update(httpheaders)
+        req = urllib2.Request(url, None, headers)
+        resp = urllib2.urlopen(req)
+        self.cookie = resp.headers.get('Set-Cookie')
+        content_length = resp.headers.get('Content-Length')
+        filename = ""
+        if resp.headers.get('Content-Disposition') is None:
+            filename = ""
+        else:
+            filename = _mid(resp.headers.get('Content-Disposition'), 'filename="','"')
+        data = resp.read()
+        if data is None:
+            _log('[*] No download provided.')
+            return ''
+        return (filename, data)
+
+    #---------------------------------------------------
+    # _mid : find substring by start and end.
+    #        >>> _mid('some text <h1>header</h1> end...', '<h1>', '</h1>')
+    #        It will return 'header'
+    #---------------------------------------------------
+    def _mid(self, s, start, end):
+        n1 = s.find(start)
+        if n1 == (-1):
+            return u''
+        n2 = s.find(end, n1 + len(start))
+        if n2 == (-1):
+            return u''
+        return s[n1 + len(start) : n2]
+
+    #---------------------------------------------------
+    # _download_ipa : Downloading of .ipa file
+    #---------------------------------------------------
+    def _download_dcn_ipa(self, url, referer, basename, **httpheaders):
+        print("[+] Downloading .ipa file from %s" % url)
+        headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate',
+                'Referer': referer,
+                'Connection': 'keep-alive'
+        }
+        headers.update(httpheaders)
+        req = urllib2.Request(url, None, headers)
+        resp = urllib2.urlopen(req)
+        re302 = resp.geturl()
+        filename = ""
+        filename = urllib.unquote(os.path.basename(re302)).decode('utf8')
+        dest_name = filename
+        data = resp.read()
+        
+        if data is None:
+            _log('[*] No download provided.')
+        else:
+            with open(dest_name, 'wb') as fw:
+                fw.write(data)
+            _log('[+] Download ok: %s' % os.path.basename(re302))
+            time.sleep(10)
---
+++ Bushido/core/dcn.py
@@ -0,0 +1,39 @@
+﻿import re,os,urllib
+from urllib2 import unquote
+
+from Bushido.common.out import _log
+from Bushido.common.curl import curl
+
+class dcn(object):
+    def __init__(self):
+        self.name = None
+        self.path = None
+        self.download_url = None
+
+    def _download(self, path, download_url):
+        self.path = path
+        
+        d = curl()
+        filename, download_html = d._curl(self.path)
+        _number_pages = re.search('<span class="total">共(.*?)页</span>', download_html, re.DOTALL)
+        _number_pages = int(_number_pages.group(1))
+        
+        try:
+            os.makedirs('./Bushido/downloads/dcn')
+        except OSError:
+            pass
+        os.chdir("./Bushido/downloads/dcn")
+        for i in range(9, _number_pages+1):
+            self.download_url = download_url + str(i) + ".html"
+            filename, download_html = d._curl(self.download_url)
+            _log("[+] Mass downloading from %s" % unquote(self.download_url))
+            found = re.findall('</a> <a href="/apps/-(.*?).html#divVedio"', download_html, re.DOTALL)
+            for _ipa_link in found:
+                _ipa_referer = "http://ios.d.cn/apps/-" + _ipa_link + ".html"
+                _log("[+] Downloading from %s" % _ipa_referer)
+                filename, download_html = d._curl(_ipa_referer)
+                found_direct = re.findall('chl=http://ios.d.cn/s-(.*?)&size=3"', download_html, re.DOTALL)
+                _download_link = "http://ios.d.cn/apps/download-" + found_direct[0] + "-web.html?js"
+                d._download_dcn_ipa(_download_link, _ipa_referer, "")
+        os.chdir('../../../')
+        
